f740308ea623e6cabc9e18dfca65e31b26d6e6ee
Halfdan Rump <dalekk@gmail.com>
2014-06-26 16:35:05 +0900

    bugfix

--- read_buttons.py
+++ read_buttons.py
@@ -13,14 +13,14 @@ ports = mapping.values()
 
 class Raspiode:
 
-	def __init__():
+	def __init__(self):
 		gpio.cleanup()
 		gpio.setmode(gpio.BCM)
 
 	def setup_ports(self):
 		for port in ports:
 			print 'Setting GPIO %s as input porn (wink wink)'%port
-			gpio.setup(porn, gpio.IN)
+			gpio.setup(port, gpio.IN)
 
 	def poll_and_print(self):
 		while True:
