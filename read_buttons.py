16d075e17573948d586a429a7bb190369854be2e
Halfdan Rump <dalekk@gmail.com>
2014-07-07 15:32:34 +0900

    Spruced up UX!

--- read_buttons.py
+++ read_buttons.py
@@ -3,6 +3,7 @@ import time
 from math import exp, log
 import operator
 import random
+import sys
 
 mapping = {
 # 	name : pi GPIO (BCM)
@@ -24,7 +25,6 @@ class Raspiode:
 
 	def setup_ports(self):
 		for port in ports:
-			print 'Setting GPIO %s as input porn (wink wink)'%port
 			gpio.setup(port, gpio.IN)
 
 	def simple_poll_and_print(self, matrix = False):
@@ -37,9 +37,16 @@ class Raspiode:
 		assert isinstance(difficulty, int) and difficulty >= 2
 		assert isinstance(speed, float)
 
-		cumul = [random.randint(-20, 20) for x in range(len(ports[:difficulty]))]
+		cumul = [random.randint(-10, 10) for x in range(len(ports[:difficulty]))]
+		cumul = map(lambda x: x * 2, cumul)
+		print "Make all the number hit zero to win!!"
+		for i in [3, 2, 1]:
+			print 'GET READY! %s'%i
+			time.sleep(1)
+		print 'GO~!!!'
 		start_time = time.time()
 
+		
 		while True:
 			gpio_input = [-1 if gpio.input(port) == 0 else 1 for port in ports[:difficulty]]
 			cumul = map(operator.add, cumul, gpio_input)
@@ -47,11 +54,11 @@ class Raspiode:
 			remaining_time = game_time - elapsed_time
 
 
-			print "%s Keep the number(s) at zero!\t"%remaining_time + "\t".join(map(lambda x: str(x), cumul[:difficulty]))
+			print "Time left: %.0f\t"%remaining_time + "\t".join(map(lambda x: str(x), cumul[:difficulty]))
 			if sum(1 for x in cumul if not x) == len(ports[:difficulty]): 
 				print 'You won, motherfucker!'
 				break
 
-			if remaining_time <= 0: print 'You lose, motherfucker!'
+			if remaining_time <= 0: print 'You lose, motherfucker!'; break
 
 			time.sleep(speed)
